0e9d2c76beb96b243af7e0886fc12842c0d0e23b
Mashikur Rahman <122012161+itsmashikur@users.noreply.github.com>
2024-07-03 14:42:21 +0600

    module suspend hook updated

--- modules/addons/sms_net_bd/hooks/AfterModuleSuspend.php
+++ modules/addons/sms_net_bd/hooks/AfterModuleSuspend.php
@@ -5,35 +5,35 @@ $hook = array(
     'description' => 'After Module Suspension',
     'type'           => 'client',
     'extra'          => '',
-    'defaultmessage' => 'Hi {firstname} {lastname},The service for your account associated with the domain {domain} has been paused.Kindly contact us for more details.',
-    'variables' => '{firstname},{lastname},{domain},{company}'
+    'defaultmessage' => 'Hi {firstname} {lastname},The service {product} has been paused.Kindly contact us for more details.',
+    'variables' => '{firstname},{lastname},{product},{domain},{company}'
 );
 
 if (!function_exists('AfterModuleSuspend')) {
     function AfterModuleSuspend($args)
     {
-        $type = $args['params']['producttype'];
-        if ($type == "hostingaccount") {
-            $class    = new Functions();
-            $template = $class->getTemplateDetails(__FUNCTION__);
-            if ($template['is_active'] == 0) {
-                return null;
-            }
-            $settings = $class->getSettings();
-            if (empty($settings['api_key'])) {
-                logActivity('sms.net.bd - AfterModuleSuspend :  ' . 'No API Key Provided', 0);
-                return null;
-            }
-        } else {
+        
+        $class    = new Functions();
+        $template = $class->getTemplateDetails(__FUNCTION__);
+        if ($template['is_active'] == 0) {
             return null;
         }
+        $settings = $class->getSettings();
+        if (empty($settings['api_key'])) {
+            logActivity('sms.net.bd - AfterModuleSuspend :  ' . 'No API Key Provided', 0);
+            return null;
+        }
+
+      
         $result   = $class->getClientDetailsBy($args['params']['clientsdetails']['userid']);
         $company_details = $class->getCompanyName();
 
         $num_rows = mysql_num_rows($result);
         if ($num_rows == 1) {
+
             $UserInformation       = mysql_fetch_assoc($result);
 
+           
             if (!$class->validatePhoneNumber($UserInformation['gsmnumber'])) {
                 logActivity('sms.net.bd - AfterModuleSuspend :  ' . 'Invalid phone number Provided', 0);
                 return null;
@@ -41,13 +41,14 @@ if (!function_exists('AfterModuleSuspend')) {
 
             $template['variables'] = str_replace(" ", "", $template['variables']);
             $replacefrom           = explode(",", $template['variables']);
-            $replaceto = array($UserInformation['firstname'], $UserInformation['lastname'], $args['params']['domain'], $company_details['CompanyName']);
+            $replaceto = array($UserInformation['firstname'], $UserInformation['lastname'], $args['params']['model']['product']['name'], $args['params']['domain'], $company_details['CompanyName']);
             $message               = str_replace($replacefrom, $replaceto, $template['content']);
             $class->setNumber($UserInformation['gsmnumber']);
             $class->setUserid($args['params']['clientsdetails']['userid']);
             $class->setMessage($message);
             $status = $class->send();
 
+           
             if ($status == 'success') {
                 logActivity('SMS Notification of Hosting Module Suspension Sent Successfully', $args['params']['clientsdetails']['userid']);
             }
